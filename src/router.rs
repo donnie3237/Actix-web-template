660b636aaf4c2427d91cdc40afea069b43580263
SoRa <paradorn3237@gmail.com>
2023-07-05 20:04:37 +0700

    .

---
+++ src/router.rs
@@ -0,0 +1,20 @@
+use actix_web::{web, HttpResponse, Responder , get ,post};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct RequestBody {
+    name: String,
+}
+
+#[get("/")]
+pub async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello worldsss!")
+}
+
+#[post("/echo")]
+pub async fn echo(req_body: web::Json<RequestBody>) -> impl Responder {
+    let name = &req_body.name;
+    let response_body = format!("Hello, {}!", name);
+
+    HttpResponse::Ok().body(response_body)
+}
